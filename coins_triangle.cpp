0d4b25615ac425c8e529985075d0afff3d04e246
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2023-11-08 10:52:42 +0530

    Add files via upload

---
+++ coins_triangle.cpp
@@ -0,0 +1,26 @@
+//Day 5: Problem Statement - Coins and Triangle
+//Topic: Binary Search
+//--------------------Solution----------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--) {
+	    int N;
+	    cin>>N;
+	    int i=1;
+	    while(N!=0) {
+	        N -= i;
+	        i++;
+	        if(N<0) {
+			   break;
+			}  
+	    }
+	    (N<0) ? cout<<i-2<<endl : cout<<i-1<<endl;
+	}
+	
+	return 0;
+}
