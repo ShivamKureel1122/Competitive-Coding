85c96360dba5819536f805307cb1c78a5bd92849
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2023-11-10 12:49:22 +0530

    Create Wildcard-Matching.cpp

---
+++ Wildcard-Matching.cpp
@@ -0,0 +1,34 @@
+//Day 6: Problem Statement - https://www.codechef.com/practice/course/strings-cpp/PCPPST01/problems/TWOSTR
+//Topic: String
+//--------------------------------Solution--------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        string X, Y;
+        cin>>X>>Y;
+        int a=0;
+        int size = X.size();
+        
+        for(int i=0; i<size; i++){
+            if( (X[i]==Y[i]) || ( (X[i]=='?') || (Y[i]=='?') ) )
+            {
+                a++;
+            }
+            else
+            {
+            	puts("No");
+            	break;
+			      }     
+        }
+        
+        if(a==size)
+           puts("Yes");
+           
+    }
+  
+	return 0;
+}
