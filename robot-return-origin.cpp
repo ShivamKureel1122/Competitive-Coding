7bf103f1809a6e0b8fbb5e837ac971c6a7caaf40
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2023-12-15 23:30:15 +0530

    Create robot-return-origin.cpp

---
+++ robot-return-origin.cpp
@@ -0,0 +1,16 @@
+Problem: https://leetcode.com/problems/robot-return-to-origin/
+==============================================================
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(int i=0; i<moves.length(); i++) {
+            if(moves[i] == 'U') x++;
+            else if(moves[i] == 'D') x--;
+            else if(moves[i] == 'R') y++;
+            else if(moves[i] == 'L') y--;
+        }
+        if((x == 0) && (y == 0)) return true;
+        else return false;
+    }
+};
