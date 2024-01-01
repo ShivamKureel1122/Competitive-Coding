943f3b372ab988f80b253b9b369e789ed41ed734
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2024-01-01 17:44:13 +0530

    Update Middle-of-LL.cpp

--- Middle-of-LL.cpp
+++ Middle-of-LL.cpp
@@ -1,14 +1,16 @@
 Problem - https://leetcode.com/problems/middle-of-the-linked-list/
-===========================================================
+-----------------------------------------------------------
 class Solution {
 public:
+    int Count(ListNode* temp) {
+        int count = 0;
+        while(temp != nullptr) { count++;
+            temp = temp->next; }
+        return count;
+    }
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head; int count = 0;
-        while(temp != nullptr) {
-            count++;
-            temp = temp->next;
-        } temp = head;
-        count = count/2;
+        ListNode* temp = head;
+        int count = Count(head)/2;
         while(count--)
             temp = temp->next;
         return temp;
