5708bcf27bf0154296dbe57094977eb73a6e3fa6
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2024-01-02 15:19:05 +0530

    Create Range-sum-of-BST.cpp

---
+++ Range-sum-of-BST.cpp
@@ -0,0 +1,18 @@
+Problem - https://leetcode.com/problems/range-sum-of-bst/
+------------------------------------------------------------
+class Solution {
+public:
+    int sum = 0;
+    void calculateSum(TreeNode* root, int low, int high) {
+        if(root != nullptr) {
+            calculateSum(root->left, low, high);
+            if(root->val >= low && root->val <= high) 
+                sum += root->val;
+            calculateSum(root->right, low, high);
+        } 
+    } 
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        calculateSum(root, low, high);
+        return sum;
+    }
+};
