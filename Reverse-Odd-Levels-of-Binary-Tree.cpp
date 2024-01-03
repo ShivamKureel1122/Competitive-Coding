d3e0bd01f4996b7dae53e629389521929076923c
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2024-01-03 23:44:53 +0530

    Create Reverse-Odd-Levels-of-Binary-Tree.cpp

---
+++ Reverse-Odd-Levels-of-Binary-Tree.cpp
@@ -0,0 +1,54 @@
+Problem - https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/
+-----------------------------------------------------------------------------------
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+    // Using a queue for level order traversal
+        queue<TreeNode*> q;
+    // Start from the root
+        q.push(root);
+    // Variable to keep track of the current level
+        int level = 0;
+    // Vector to store nodes at each level for later processing
+        vector<TreeNode*> v;
+    // Vector to store values of nodes at odd levels for reversal
+        vector<int> temp;
+
+    // Perform level order traversal
+        while (!q.empty()) {
+            int nodeCount = q.size();
+            while (nodeCount--) {
+            // Process each node at the current level
+                TreeNode* curNode = q.front();
+                q.pop();
+            // Store the node for later processing
+                v.push_back(curNode);
+            // Enqueue left and right children if they exist
+                if (curNode->left)
+                    q.push(curNode->left);
+                if (curNode->right)
+                    q.push(curNode->right);
+            // If the current level is odd, store the value for reversal
+                if (level % 2)
+                    temp.push_back(curNode->val);
+            }
+
+        // Reverse the values at odd levels
+            reverse(temp.begin(), temp.end());
+
+        // Update the values of nodes at odd levels
+            for (int i = 0; i < temp.size(); i++) 
+                v[i]->val = temp[i];
+
+        // Clear vectors for the next level
+            v.clear();
+            temp.clear();
+
+        // Move to the next level
+            level++;
+        }
+
+    // Return the modified root of the binary tree
+        return root;
+    }
+};
